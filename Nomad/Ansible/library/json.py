ede3c7a839c1909b3f363c0d8859a20b6be84ac1
Antonio Alférez <aj.alferez@outlook.com>
2016-05-13 12:44:24 +0200

    Fix update config script

--- Nomad/Ansible/library/json.py
+++ Nomad/Ansible/library/json.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
 import json
-from sys import argv
+from os import path
 
 def main():
     module = AnsibleModule(
         argument_spec = dict(
+            file = dict(required=True),
             path = dict(required=True),
-            name = dict(required=True, choices=['node_name', 'name'])
+            url = dict(required=True),
+            key = dict(required=True, choices=['node_name', 'name']),
+            name = dict(required=True),
         )
     )
-    filename = modules.params['path']
+
+    filename = os.path.join(module.params['path'],module.params['file'])
     # Read
     with open(filename,'r') as data_file:
         data = json.load(data_file)
 
+    key = module.params['key']
+    if key not in data:
+        module.fail_json(msg="No key: "+key)
+
     # Change
-    data["bind_addr"] = {{ ansible_default_ipv4.address }}
-    data[module.params['name']] = {{ansible_nodename}}
+    data["bind_addr"] = module.params['url']
+    data[key] = module.params['name']
 
     # Write
     with open(filename, 'w') as f:
          json.dump(data, f)
 
     module.exit_json(changed=True)
-    module.fail_json(msg="Something fatal happened")
+
 
 # Execute
 from ansible.module_utils.basic import *
