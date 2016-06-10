eb8283b2375c689ec360a76b8e8a17b0bcab4f0e
Antonio Alférez <aj.alferez@outlook.com>
2016-06-10 03:09:18 +0200

    Change load bounds

--- Nagios/scripts/install.sh
+++ Nagios/scripts/install.sh
@@ -11,9 +11,9 @@ sudo make install
 cd ~
 wget https://sourceforge.net/projects/nagios/files/nrpe-2.x/nrpe-2.15/nrpe-2.15.tar.gz
 tar xzf nrpe-2.15.tar.gz
-cd nrpe-2.8
+cd nrpe-2.15
 sudo apt-get install -y libssl-dev
 ./configure --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
 make all
-sudo make install
+sudo make install-plugin
 # /usr/local/nagios/libexec
